72d15c0adfcfe14a8cd1e5c31b7767d9615ffe03
BuianAlex <a.buyan@ukr.net>
2019-10-25 02:54:58 +0300

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import './App.scss';
-import Tami from './modules/tami'
+
+//import Tami from './modules/tami'
+import { join } from 'path';
 
 class App extends React.Component {
+  
   constructor(props) {
     super(props);
+    this.moodImg = ["tami-norm","51tami","tami-sick","tami-sleep","tami-died"]
     this.state = { 
       inputName: '',
-      tami: {id: 1, name: "dfdfdfd", health: 100, fullness: 100,happiness: 100, lifeTime: 1000, mood: 0},
-
-      // allTami: [
-      //    {id: 1, name: "dfdfdfd", health: 100, fullness: 100,happiness: 100, , mood: 0},
-      //   // {name: "Qiqi", health: 99, fullness: 80,happiness: 100, lifeTime: 1000, mood: 1},
-      //   // {name: "MimiMimiMimiMimiMimi", health: 50, fullness: 60,happiness: 101, lifeTime: 999, mood: 2},
-      //   // {name: "Mimi", health: 50, fullness: 60,happiness: 101, lifeTime: 999, mood: 3},
-      //   // {name: "Mimi", health: 50, fullness: 60,happiness: 101, lifeTime: 999, mood: 0},
-      //   // {name: "Mimi", health: 50, fullness: 60,happiness: 101, lifeTime: 999, mood: 2},
-      // ], 
+      name: "sdfs ",
+      health: 100,
+      healthBtnState: '',
+      fullness: 110,
+      fullnessBtnState: '',
+      happiness: 100,
+      happinessBtnState: '',
+      wisdom: 25,
+      wisdomBtnState: '',
+      workout: 0,
+      workoutBtnState: '',
+      sleep: false,
+      sleepBtnState: '',
+      lifeTime: 1700, 
+      mood: 0 
     };
+    this.lastCureAction = 0;
+    //this.dayTime = 3600; //1Day === 1 hour rial
+    this.dayTime = 3600; //1Day === 1 hour rial
   }
   inputChangeHandler = (event) => {
     const wrong = /[!@#$%^&*(),.?":{}|<>]/g
@@ -28,47 +40,318 @@ class App extends React.Component {
     }
   }
   componentDidMount(){
-    if(this.state.tami.name !== " "){
+    if(this.state.name !== " "){
       this.lifeTimer();   
-  }
     }
+  }
       
   
   createTami(e){
     e.preventDefault();
     this.setState({
-      tami: {
-        id: (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase(),
-        name: this.state.inputName,
-        health: 100, 
-        fullness: 100,
-        happiness: 100, 
-        lifeTime: 3000, 
-        mood: 0}
-    })
+      inputName: '',
+      name: this.state.inputName,
+      health: 100,
+      healthBtnState: '',
+      fullness: 100,
+      fullnessBtnState: '',
+      happiness: 100,
+      happinessBtnState: '',
+      wisdom: 0,
+      wisdomBtnState: '',
+      workout: 0,
+      workoutBtnState: '',
+      sleep: false,
+      sleepBtnState: '',
+      lifeTime: 0, 
+      mood: 0 }
+    )
     this.lifeTimer();
   }
+  
   lifeTimer() {
     this.timer = setInterval(() =>{
-      let temp =  Object.assign(this.state.tami);
-      temp.lifeTime += 1;
-      console.log(temp  );
       this.setState({
-        tami: temp,
+        lifeTime: this.state.lifeTime +1 ,
       })
-      console.log("hard bit"+this.state.tami);
+        this.feedControl();
+        this.moodControl();
+        this.happinessControl();
+        this.wisdomControl();
+        this.healthControl();
+        this.workoutControl();
+        this.cureControl();
+      console.log("heartbeat "+JSON.stringify(this.state));
     }, 1000 )
+  }
+  //кожні 15хв -1% їжі
+  //якщо спить -100% за день
+  feedControl(amount, btn = false) {
+    let curState = this.state.fullness;
+    if(this.state.lifeTime % (this.dayTime*0.25/100) === 0 && !this.state.sleep){
+      curState -= 1;
+      console.log("feedControl by time");
+    }
+    if(this.state.lifeTime % (this.dayTime/100) === 0 && this.state.sleep){
+       curState -= 1;
+       console.log("feedControl by time sl");
+    }
+    if(btn){
+      curState += amount;
+    }
+    this.setState({fullness: curState });
+  }
+
+  moodControl() {
+    if(
+      this.state.fullness >=  30 && this.state.fullness < 50 ||
+      this.state.happiness >= 30 && this.state.happiness <50 ||
+      this.state.health >= 30 && this.state.health < 50
+      )
+    {
+      this.setState({mood: 1 });
+    }
+    else if(
+      this.state.fullness >=  50  && this.state.happiness >= 50 && this.state.health >= 50) {
+      this.setState({mood: 0 });
+    }
+    else{
+      this.setState({mood: 2 });
+    }
+    if( this.state.health <= 0){
+        this.setState({mood: 4 });
+    }
+    if(this.state.sleep){
+      this.setState({mood: 3});
+    }
+  }
+
+  happinessControl(amount, btn = false) {
+    let curState = this.state.happiness;
+    if(this.state.lifeTime % (this.dayTime / 100) === 0){
+      if(this.state.sleep){
+        curState += 1;
+      }
+      else{
+        curState -= 1;
+        if(this.state.fullness <  30){
+          curState -= 5;
+        }
+        if(this.state.health <  50){
+          curState -= 5;
+        }
+      }
+    }
+    if(btn) {
+      curState += amount ;
+    }
+    this.setState({happiness: curState });
+  }
+  
+  //авто проверка раз в час 
+  healthControl(amount, btn = false) {
+    let curState = this.state.health;
+    if(this.state.fullness <=  0 || this.state.happiness <= 0||this.state.fullness > 110) {
+      curState -= 1;
+    }           
+    if(btn){
+      curState += amount;
+    }
+    if(this.state.health <= 0){
+      this.setState({
+        wisdomBtnState: " disabled",
+        healthBtnState: " disabled",
+        fullnessBtnState: " disabled",
+        happinessBtnState: " disabled",
+        workoutBtnState: " disabled",
+        sleepBtnState: " disabled",
+      })
+      clearTimeout(this.timer);
+    }
+    this.setState({health:  curState < 100 ? curState >=0?curState: 0 : 100 });
+  }
+
+  cureControl(){
+    if (this.lastCureAction === 0  || this.lifeTime - this.lastCureAction > this.dayTime) {
+      if (!this.state.sleep){
+        this.setState({healthBtnState: ""});
+      }
+    }
+  }
+
+  wisdomControl(amount, btn=false) {
+    if(this.state.fullness >= 100 && this.state.health >= 100 && this.state.happiness >= 100 ) {
+      this.setState({wisdomBtnState: ''});
+      if(btn){
+        this.setState({
+          wisdom: this.state.wisdom += 5,
+          happiness: this.state.happiness -= 5,  
+        });
+      }
+
+    }
+    else{
+      this.setState({wisdomBtnState: ' disabled'});
+    }
+    let toHappy =  this.state.happiness - 100;
+    let tofullness = this.state.fullness - 100
+    switch (this.state.wisdom) {
+      case 25:
+        console.log('auto25');
+        
+        if(toHappy < 0) {
+          console.log("auto happy 25");
+          this.gameAction();
+        }
+        break;
+      case 50:
+        if(toHappy < 0) {
+          console.log("auto happy 50");
+          this.gameAction();
+        }
+        if(this.state.workout === 0){
+          this.workoutAction();
+          console.log("workout50.1");
+          setTimeout(function(){ console.log("workout50.2"); }, 1800000);
+        }
+        if (this.state.workout === 50){
+          this.workoutAction();
+        }
+        break;
+      case 75:
+          if(toHappy < 0) {
+            console.log("auto happy 50");
+            this.gameAction();
+          }
+          if(this.state.workout === 0){
+            this.workoutAction();
+            console.log("workout50.1");
+            setTimeout(function(){ console.log("workout50.2"); }, 1800000);
+          }
+          if (this.state.workout === 50){
+            console.log("workout50.1");
+            this.workoutAction();
+          }
+          this.cureAction();
+          break;
+      case 100:
+          if(toHappy < 0) {
+            console.log("auto happy 50");
+            this.gameAction();
+          }
+          if(this.state.workout === 0){
+            this.workoutAction();
+            console.log("workout50.1");
+            setTimeout(function(){ console.log("workout50.2"); }, 1800000);
+          }
+          if (this.state.workout === 50){
+            console.log("workout50.1");
+            this.workoutAction();
+          }
+          this.cureAction();
+          this.feedControl(tofullness*-1, true);
+        break    
+      default:
+        break;
+    }
+  }
+
+  workoutControl(amount, btn=false){
+    let curState = this.state.workout;
+    if(this.state.lifeTime % this.dayTime === 0) {
+      switch (curState) {
+        case 0:
+          this.healthControl(-1, true);
+          break;
+        case 50:
+          break;
+        case 100:  
+          this.healthControl(1, true);
+          break;
+        default:
+          break;
+      }
+      if(this.state.workout > 100) {
+        this.happinessControl(-5, true);
+      }
+      curState = 0;
+    }
+    if(btn){
+      curState += amount;
+    }
+    this.setState({workout: curState});
   }
 
   killTami(){
     this.setState({
-      tami: {},
+      name: " ",
+      health: 0,
+      fullness: 0,
+      happiness: 0,
+      lifeTime: 0, 
+      mood: 0 ,
     })
     clearTimeout(this.timer);
   }
+  feedAction(){
+    if(!this.state.sleep){
+      this.feedControl(30, true);
+    }
+  } 
+  gameAction(){
+    if(!this.state.sleep){
+      this.happinessControl(30, true);
+      this.feedControl(-20, true);
+    }
+  }
+  workoutAction(){
+    if(!this.state.sleep){
+      this.workoutControl(50, true);
+      this.happinessControl(10, true);
+      this.feedControl(-20, true);
+    } 
+  }
+  cureAction(){
+    if (this.state.healthBtnState ==='' && !this.state.sleep) {
+      this.happinessControl(-20, true);
+      this.healthControl(20, true);
+      this.lastCureAction = this.lifeTime;
+      this.setState({healthBtnState: " disabled"});
+    }
+
+  }
+  wisdomAction() {
+    this.wisdomControl("",true);
+  }
+  
+  sleepAction(){
+    this.setState({});
+    if(!this.state.sleep){
+      this.setState({
+        sleep: true,
+        wisdomBtnState: " disabled",
+        healthBtnState: " disabled",
+        fullnessBtnState: " disabled",
+        happinessBtnState: " disabled",
+        workoutBtnState: " disabled",
+        sleepBtnState: " sleeping-btn",
+      })
+    }
+    else{
+      this.setState({
+        sleep: false,
+        wisdomBtnState: "",
+        // healthBtnState: "",
+        fullnessBtnState: "",
+        happinessBtnState: "",
+        workoutBtnState: "",
+        sleepBtnState: "",
+      })
+    }
+  } 
 
   render() {
-    if(Object.entries(this.state.tami).length === 0) {
+    if(this.state.name === " ") {
       return (
         <div className="welcomeScreen">
           <div className="iconWrap">
@@ -90,16 +373,27 @@ class App extends React.Component {
         <div>
           <div className="action-menu">
             {/* <button className="add-new">Add a new friend</button> */}
-            <button className="circle-btn feed-btn"></button>
-            <button className="circle-btn cure-btn"></button>
-            <button className="circle-btn fun-btn"></button>
-            <button className="circle-btn teach-btn"></button>
-            <button className="circle-btn walk-btn"></button>
-            <button className="circle-btn sleep-btn"></button>
+            <button className= {'circle-btn feed-btn'+this.state.fullnessBtnState} onClick={this.feedAction.bind(this)}></button>
+            <button className= {"circle-btn cure-btn"+this.state.healthBtnState} onClick={this.cureAction.bind(this)}></button>
+          <button className={"circle-btn fun-btn"+this.state.happinessBtnState} onClick={this.gameAction.bind(this)}></button>
+            <button className={"circle-btn teach-btn"+this.state.wisdomBtnState} onClick={this.wisdomAction.bind(this)}></button>
+            <button className={"circle-btn walk-btn"+this.state.workoutBtnState} onClick={this.workoutAction.bind(this)} ></button>
+            <button className={"circle-btn sleep-btn"+this.state.sleepBtnState} onClick={this.sleepAction.bind(this)}></button>
             <button className="circle-btn kill-btn"  onClick={this.killTami.bind(this)}></button>
           </div>
           <ul className="tami-list">
-            <Tami dataProps = {this.state.tami}></Tami>
+            <li className = {`tami ${this.state.active}`} key='{this.props.key}' >
+              <img src={`./img/${this.moodImg[this.state.mood]}.png`}></img>
+              <div className="tami-name">{this.state.name}</div>
+              <div className="tami-time">{Math.floor(this.state.lifeTime / 3600)} day</div>
+              <ul className="indicators">
+                  <li className="health">{this.state.health}</li>
+                  <li className="fullness">{this.state.fullness}</li>
+                  <li className="happiness">{this.state.happiness}</li>
+                  <li className="wisdom">{this.state.wisdom}</li>
+                  <li className="workout">{this.state.workout}</li>
+              </ul>
+            </li>
           </ul>
              
         </div>
